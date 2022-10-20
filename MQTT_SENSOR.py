0da8060f5fed3c8b8dc3878f2aadb57cc628cb19
PeOpLeRu <100120835+PeOpLeRu@users.noreply.github.com>
2022-10-20 17:40:10 +0800

    add

---
+++ MQTT_SENSOR.py
@@ -0,0 +1,95 @@
+#  Publisher
+
+from operator import ne
+import serial, serial.tools.list_ports
+import paho.mqtt.client as mqtt_client
+import time, random
+import numpy as np
+
+def connect():
+    print("Список доступных портов:")
+    ports = []
+    for elem in serial.tools.list_ports.comports():
+        print(str(elem).split(' ')[0])
+        ports += [str(elem).split(' ')[0]]
+
+    port = "COM" + input("Выберите порт COM:")
+    if port not in ports:
+        print(f"Port {port} in not exists!")
+        return -1
+    
+    s = serial.Serial(port=port, baudrate=9600)
+    if not s.is_open:
+        print(f"Connection failure!")
+        return -2
+
+    return s
+
+def map(value, in_min, in_max, out_min, out_max):
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+broker = "broker.emqx.io"
+UNIQUE_ID = 998
+
+client = mqtt_client.Client(f'lab_{random.randint(10000, 999999)}')
+
+client.connect(broker)
+
+# arduino = serial.Serial(port="COM5", baudrate=9600)
+arduino = connect()
+
+print("""Список команд:
+1 - Получить моментальное значение сенсора
+2 - Получить усредненное значение по 100 последним элементам
+3 - Реализация потоковой передачи данных (default duration 20 seconds)
+""")
+
+duration = 20
+queue = []
+avg_value = 0
+need_input = True
+
+while True:
+    if need_input:
+        command = int(input("\rВведите номер команды: "))
+
+    if (command == 3):
+        temp = input("Введите продолжительность в секундах (enter for old value): ")
+        if temp != '':
+            duration = int(temp)
+    elif command == 0:
+        break
+    
+    arduino.write(np.array([1], dtype='uint8').tobytes())
+    
+    time.sleep(0.01)
+
+    while arduino.inWaiting() < 2:
+        print("\rWaiting data", end="")
+
+    response = arduino.read(2)  # Прочитали
+    response = [int(byte_) for byte_ in response]   # массив байт в массив int8
+    response = (response[0] << 8 & 0xFF00) + (response[1] & 0xFF)   # Взяли число
+    response = map(response, 0, 1024, 0, 100)   # Смена диапазона
+    queue = [response] + queue  # Фиксируем новое значение
+
+    avg_value += response   # Рассчёты для среднего по последним 100 значениям
+    if len(queue) >= 100:
+        avg_value -= queue.pop()
+
+    if command == 1:
+        client.publish(f'lab/{UNIQUE_ID}/photo/instant', response) 
+    elif command == 2:
+        client.publish(f'lab/{UNIQUE_ID}/photo/instant', avg_value / len(queue)) 
+    elif command == 3:
+        need_input = False
+        timer_start = time.time()
+        command = 4
+    elif command == 4:
+        print("\rSending data", end="")
+        if time.time() - timer_start >= duration:
+            need_input = True
+        client.publish(f'lab/{UNIQUE_ID}/photo/instant', response)         
+
+print("Disconnect!")
+client.disconnect()
